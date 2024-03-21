78bd7041d94a1d2212cd82a51a2f852908bdcf72
Joel Munuera Marrero <joel.munu@gmail.com>
2024-03-21 12:20:02 +0000

    Refactor: Small changes have been made

--- src/main/java/com/iocexample/factory/Factory.java
+++ src/main/java/com/iocexample/factory/Factory.java
@@ -13,8 +13,6 @@ public class Factory {
 		
 	}
 	
-	
-	
 	public IEmployee getEmployee() {
 		return employee;
 	}
--- src/main/java/com/iocexample/main/Main.java
+++ src/main/java/com/iocexample/main/Main.java
@@ -1,22 +1,20 @@
 package com.iocexample.main;
 
-import com.iocexample.employee.Employee;
-
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import com.iocexample.employee.Ceo;
 import com.iocexample.factory.Factory;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
 		BeanFactory beanFactory = context;
+		System.out.println("--- Reporte de trabajo del día ---");
 		Factory factory = (Factory) beanFactory.getBean("factory");
 		factory.getOnDuty();
 		factory.getOffDuty();
-	}
 
+		context.close();
+	}
 }
